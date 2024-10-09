9e29a456c46f8616bb486d9a8be7f0fc4df164cd
Ade Rahmat Maulana <adermaulana15@gmail.com>
2024-10-10 00:23:52 +0800

    tambah data

--- admin/index.php
+++ admin/index.php
@@ -20,7 +20,7 @@ if($_SESSION['status'] != 'login'){
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Spike Free</title>
+  <title>Dahsboard Admin</title>
   <link rel="shortcut icon" type="image/png" href="../assets/images/logos/favicon.png" />
   <link rel="stylesheet" href="../assets/css/styles.min.css" />
 </head>
@@ -34,7 +34,7 @@ if($_SESSION['status'] != 'login'){
       <!-- Sidebar scroll-->
       <div class="scroll-sidebar" data-simplebar>
         <div class="d-flex mb-4 align-items-center justify-content-between">
-            <a href="index.html" class="text-nowrap logo-img ms-0 ms-md-1">
+            <a href="index.php" class="text-nowrap logo-img ms-0 ms-md-1">
               <img src="../assets/images/logos/dark-logo.svg" width="180" alt="">
             </a>
             <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
@@ -51,7 +51,7 @@ if($_SESSION['status'] != 'login'){
             <li class="sidebar-item">
               <a
                 class="sidebar-link sidebar-link primary-hover-bg"
-                href="./index.html"
+                href="index.php"
                 aria-expanded="false"
               >
                 <span class="aside-icon p-2 bg-light-primary rounded-3">
@@ -62,178 +62,59 @@ if($_SESSION['status'] != 'login'){
             </li>
             <li class="nav-small-cap">
               <i class="ti ti-dots nav-small-cap-icon fs-5"></i>
-              <span class="hide-menu">UI Componenst</span>
+              <span class="hide-menu">Fitur</span>
             </li>
             <li class="sidebar-item">
               <a
                 class="sidebar-link sidebar-link warning-hover-bg"
-                href="./ui-buttons.html"
+                href="user.php"
                 aria-expanded="false"
               >
                 <span class="aside-icon p-2 bg-light-warning rounded-3">
                   <i class="ti ti-article fs-7 text-warning"></i>
                 </span>
-                <span class="hide-menu ms-2 ps-1">Buttons</span>
+                <span class="hide-menu ms-2 ps-1">Data User</span>
               </a>
             </li>
             <li class="sidebar-item">
               <a
                 class="sidebar-link sidebar-link danger-hover-bg"
-                href="./ui-alerts.html"
+                href="kelas.php"
                 aria-expanded="false"
               >
                 <span class="aside-icon p-2 bg-light-danger rounded-3">
                   <i class="ti ti-alert-circle fs-7 text-danger"></i>
                 </span>
-                <span class="hide-menu ms-2 ps-1">Alerts</span>
+                <span class="hide-menu ms-2 ps-1">Data Kelas</span>
               </a>
             </li>
             <li class="sidebar-item">
               <a
                 class="sidebar-link sidebar-link success-hover-bg"
-                href="./ui-card.html"
+                href="pembayaran.php"
                 aria-expanded="false"
               >
                 <span class="aside-icon p-2 bg-light-success rounded-3">
                   <i class="ti ti-cards fs-7 text-success"></i>
                 </span>
-                <span class="hide-menu ms-2 ps-1">Card</span>
+                <span class="hide-menu ms-2 ps-1">Data Pembayaran</span>
               </a>
             </li>
             <li class="sidebar-item">
               <a
                 class="sidebar-link sidebar-link primary-hover-bg"
-                href="./ui-forms.html"
+                href="sistem.php"
                 aria-expanded="false"
               >
                 <span class="aside-icon p-2 bg-light-primary rounded-3">
                   <i class="ti ti-file-description fs-7 text-primary"></i>
                 </span>
-                <span class="hide-menu ms-2 ps-1">Forms</span>
+                <span class="hide-menu ms-2 ps-1">Sistem Aplikasi</span>
               </a>
             </li>
-            <li class="sidebar-item">
-              <a
-                class="sidebar-link sidebar-link indigo-hover-bg"
-                href="./ui-typography.html"
-                aria-expanded="false"
-              >
-                <span class="aside-icon p-2 bg-light-indigo rounded-3">
-                  <i class="ti ti-typography fs-7 text-indigo"></i>
-                </span>
-                <span class="hide-menu ms-2 ps-1">Typography</span>
-              </a>
-            </li>
-            <li class="nav-small-cap">
-              <i class="ti ti-dots nav-small-cap-icon fs-5"></i>
-              <span class="hide-menu">Auth</span>
-            </li>
-            <li class="sidebar-item">
-              <a
-                class="sidebar-link sidebar-link warning-hover-bg"
-                href="./authentication-login.html"
-                aria-expanded="false"
-              >
-                <span class="aside-icon p-2 bg-light-warning rounded-3">
-                  <i class="ti ti-login fs-7 text-warning"></i>
-                </span>
-                <span class="hide-menu ms-2 ps-1">Login</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a
-                class="sidebar-link sidebar-link danger-hover-bg"
-                href="./authentication-register.html"
-                aria-expanded="false"
-              >
-                <span class="aside-icon p-2 bg-light-danger rounded-3">
-                  <i class="ti ti-user-plus fs-7 text-danger"></i>
-                </span>
-                <span class="hide-menu ms-2 ps-1">Register</span>
-              </a>
-            </li>
-            <li class="nav-small-cap">
-              <i class="ti ti-dots nav-small-cap-icon fs-5"></i>
-              <span class="hide-menu">Extra</span>
-            </li>
-            <li class="sidebar-item">
-              <a
-                class="sidebar-link sidebar-link success-hover-bg"
-                href="./icon-tabler.html"
-                aria-expanded="false"
-              >
-                <span class="aside-icon p-2 bg-light-success rounded-3">
-                  <i class="ti ti-mood-happy fs-7 text-success"></i>
-                </span>
-                <span class="hide-menu ms-2 ps-1">Icons</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a
-                class="sidebar-link sidebar-link primary-hover-bg"
-                href="./sample-page.html"
-                aria-expanded="false"
-              >
-                <span class="aside-icon p-2 bg-light-primary rounded-3">
-                  <i class="ti ti-aperture fs-7 text-primary"></i>
-                </span>
-                <span class="hide-menu ms-2 ps-1">Sample Page</span>
-              </a>
-            </li>
+
           </ul>
-          <div class="pb-3 options text-nowrap">
-            <div class="nav-small-cap">
-              <i class="ti ti-dots nav-small-cap-icon fs-5"></i>
-              <span class="hide-menu">More options</span>
-            </div>
 
-            <ul class="sidebar-list">
-              <li class="sidebar-list-item">
-                <i class="ti ti-circle text-primary fs-4"></i>
-                <span class="hide-menu ms-2">Applications</span>
-              </li>
-              <li class="sidebar-list-item">
-                <i class="ti ti-circle text-danger fs-4"></i>
-                <span class="hide-menu ms-2">Form Options</span>
-              </li>
-              <li class="sidebar-list-item">
-                <i class="ti ti-circle text-warning fs-4"></i>
-                <span class="hide-menu ms-2">Table Variations</span>
-              </li>
-              <li class="sidebar-list-item">
-                <i class="ti ti-circle text-success fs-4"></i>
-                <span class="hide-menu ms-2">Charts Selection</span>
-              </li>
-              <li class="sidebar-list-item">
-                <i class="ti ti-circle text-indigo fs-4"></i>
-                <span class="hide-menu ms-2">Widgets</span>
-              </li>
-            </ul>
-          </div>
-
-          <div class="mt-5 blocks-card sidebar-ad">
-            <div class="card bg-light-primary">
-              <div class="card-body">
-                <div class="text-center">
-                  <img
-                    src="../assets/images/backgrounds/education-blocks.png"
-                    width="136"
-                    height="136"
-                    class="mt-n9"
-                    alt=""
-                  />
-
-                  <h5>Are you<br/> satisfied ?</h5>
-
-                  <div class="mt-4">
-                    <a href="" target="_blank" class="btn btn-primary buynow-link w-100 px-2">
-                      Buy Now
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
         </nav>
         <!-- End Sidebar navigation -->
       </div>
@@ -260,7 +141,6 @@ if($_SESSION['status'] != 'login'){
           </ul>
           <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
             <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
-              <a href="https://adminmart.com/product/Spike-free-bootstrap-admin-dashboard/" target="_blank" class="btn btn-primary">Download Free</a>
               <li class="nav-item dropdown">
                 <a class="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2" data-bs-toggle="dropdown"
                   aria-expanded="false">

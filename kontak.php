228e5df0c6e3de12f31f99f9cb3af51348c8c1f6
Ade Rahmat Maulana <adermaulana15@gmail.com>
2024-10-20 12:45:17 +0800

    bimbel

--- kontak.php
+++ kontak.php
@@ -87,11 +87,7 @@
     </div><!-- End Page Title -->
 
     <!-- Contact Section -->
-    <section id="contact" class="contact section">
-
-      <div class="mb-5" data-aos="fade-up" data-aos-delay="200">
-        <iframe style="border:0; width: 100%; height: 300px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d48389.78314118045!2d-74.006138!3d40.710059!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a22a3bda30d%3A0xb89d1fe6bc499443!2sDowntown%20Conference%20Center!5e0!3m2!1sen!2sus!4v1676961268712!5m2!1sen!2sus" frameborder="0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-      </div><!-- End Google Maps -->
+    <section id="contact" class="contact section mt-5">
 
       <div class="container" data-aos="fade-up" data-aos-delay="100">
 
@@ -101,8 +97,8 @@
             <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="300">
               <i class="bi bi-geo-alt flex-shrink-0"></i>
               <div>
-                <h3>Address</h3>
-                <p>A108 Adam Street, New York, NY 535022</p>
+                <h3>Jalan Perintis</h3>
+                <p>Makassar, Indonesia</p>
               </div>
             </div><!-- End Info Item -->
 
@@ -110,7 +106,7 @@
               <i class="bi bi-telephone flex-shrink-0"></i>
               <div>
                 <h3>Call Us</h3>
-                <p>+1 5589 55488 55</p>
+                <p>+62 853 55488 55</p>
               </div>
             </div><!-- End Info Item -->
 
@@ -118,7 +114,7 @@
               <i class="bi bi-envelope flex-shrink-0"></i>
               <div>
                 <h3>Email Us</h3>
-                <p>info@example.com</p>
+                <p>bimbel@example.com</p>
               </div>
             </div><!-- End Info Item -->
 
@@ -169,20 +165,14 @@
     <div class="container footer-top">
       <div class="row gy-4">
         <div class="col-lg-4 col-md-6 footer-about">
-          <a href="index.html" class="logo d-flex align-items-center">
+          <a href="index.php" class="logo d-flex align-items-center">
             <span class="sitename">Mentor</span>
           </a>
           <div class="footer-contact pt-3">
-            <p>A108 Adam Street</p>
-            <p>New York, NY 535022</p>
-            <p class="mt-3"><strong>Phone:</strong> <span>+1 5589 55488 55</span></p>
-            <p><strong>Email:</strong> <span>info@example.com</span></p>
-          </div>
-          <div class="social-links d-flex mt-4">
-            <a href=""><i class="bi bi-twitter-x"></i></a>
-            <a href=""><i class="bi bi-facebook"></i></a>
-            <a href=""><i class="bi bi-instagram"></i></a>
-            <a href=""><i class="bi bi-linkedin"></i></a>
+            <p>Jalan Perintis</p>
+            <p>Makassar, Indonesia</p>
+            <p class="mt-3"><strong>Phone:</strong> <span>+62 853 55488 55</span></p>
+            <p><strong>Email:</strong> <span>bimbel@example.com</span></p>
           </div>
         </div>
 

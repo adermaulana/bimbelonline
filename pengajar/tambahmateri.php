44300b58d388bc0ae3977ff576b2167d4f465964
Ade Rahmat Maulana <adermaulana15@gmail.com>
2024-10-20 12:35:41 +0800

    pengajar dan siswa

--- pengajar/tambahmateri.php
+++ pengajar/tambahmateri.php
@@ -164,139 +164,46 @@ if ($_SESSION['role_admin'] != 'pengajar') {
       </header>
       <!--  Header End -->
       <div class="container-fluid">
-        <!--  Row 1 -->
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="row">
-              <div class="col-lg-6 col-sm-6">
-                <!-- Yearly Breakup -->
-                <div class="card overflow-hidden">
-                  <div class="card-body p-4">
-                    <h5 class="card-title mb-10 fw-semibold">Total Pembelian</h5>
-                    <div class="row align-items-center">
-                      <div class="col-7">
-                        <h4 class="fw-semibold mb-3">12</h4>
-                        <div class="d-flex align-items-center mb-2">
-                          <span
-                            class="me-1 rounded-circle bg-light-success round-20 d-flex align-items-center justify-content-center">
-                            <i class="ti ti-arrow-up-left text-success"></i>
-                          </span>
-                          <p class="text-dark me-1 fs-3 mb-0">+9%</p>
-                          <p class="fs-3 mb-0">last year</p>
-                        </div>
-                        <div class="d-flex align-items-center">
-                          <div class="me-3">
-                            <span class="round-8 bg-primary rounded-circle me-2 d-inline-block"></span>
-                            <span class="fs-2">Oragnic</span>
-                          </div>
-                          <div>
-                            <span class="round-8 bg-danger rounded-circle me-2 d-inline-block"></span>
-                            <span class="fs-2">Refferal</span>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="col-5">
-                        <div class="d-flex justify-content-center">
-                          <div id="grade"></div>
-                        </div>
-                      </div>
+        <div class="container-fluid">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title fw-semibold mb-4">Tambah Materi</h5>
+              <div class="card">
+                <div class="card-body col-6">
+                  <form method="POST">
+                    <div class="mb-3">
+                      <label for="name_221047" class="form-label">Judul Materi</label>
+                      <input type="text" class="form-control" id="name_221047" name="name_221047" required>
                     </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-lg-6 col-sm-6">
-                <!-- Monthly Earnings -->
-                <div class="card">
-                  <div class="card-body">
-                    <div class="row alig n-items-start">
-                      <div class="col-8">
-                        <h5 class="card-title mb-10 fw-semibold">Total Pendapatan</h5>
-                        <h4 class="fw-semibold mb-3">Rp 250.000.000</h4>
-                        <div class="d-flex align-items-center pb-1">
-                          <span
-                            class="me-2 rounded-circle bg-light-danger round-20 d-flex align-items-center justify-content-center">
-                            <i class="ti ti-arrow-down-right text-danger"></i>
-                          </span>
-                          <p class="text-dark me-1 fs-3 mb-0">+9%</p>
-                          <p class="fs-3 mb-0">last year</p>
-                        </div>
-                      </div>
-                      <div class="col-4">
-                        <div class="d-flex justify-content-end">
-                          <div
-                            class="text-white bg-danger rounded-circle p-7 d-flex align-items-center justify-content-center">
-                            <i class="ti ti-currency-dollar fs-6"></i>
-                          </div>
-                        </div>
-                      </div>
+                    <div class="mb-3">
+                        <label for="role_221047" class="form-label">Kelas</label>
+                        <select class="form-select" id="role_221047" name="role_221047" required>
+                            <option value="" disabled selected>Pilih Kelas</option>
+                            <option value="admin">Web Development</option>
+                            <option value="pengajar">Fullstack Developer</option>
+                            <option value="siswa">Backend Engineer</option>
+                            <!-- Tambahkan opsi lain sesuai kebutuhan -->
+                        </select>
                     </div>
-                  </div>
-                  <div id="earning"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-lg-12 d-flex align-items-stretch">
-            <div class="card w-100">
-              <div class="card-body p-4">
-                <div
-                  class="d-flex mb-4 justify-content-between align-items-center"
-                >
-                  <h5 class="mb-0 fw-bold">Daftar Pembeli Paket dan Materi</h5>
-                </div>
+                    <div class="mb-3">
+                      <label for="phone_221047" class="form-label">Deskripsi</label>
+                      <input type="date" class="form-control" id="phone_221047" name="phone_221047" required>
+                    </div>
+                    <div class="mb-3">
+                      <label for="phone_221047" class="form-label">File</label>
+                      <input type="file" class="form-control" id="phone_221047" name="phone_221047" required>
+                    </div>
+                    <div class="mb-3">
+                      <label for="phone_221047" class="form-label">Deskripsi</label>
+                      <textarea class="form-control" name="" id="" rows="4"></textarea>
+                    </div>
 
-                <div class="table-responsive" data-simplebar>
-                  <table
-                    class="table table-borderless align-middle text-nowrap"
-                  >
-                    <thead>
-                      <tr>
-                        <th scope="col">Foto</th>
-                        <th scope="col">Nama Pembeli</th>
-                        <th scope="col">Jenis Pembelian</th>
-                        <th scope="col">Status</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>
-                          <div class="d-flex align-items-center">
-                            <div class="me-4">
-                              <img
-                                src="../assets/images/profile/user1.jpg"
-                                width="50"
-                                class="rounded-circle"
-                                alt=""
-                              />
-                            </div>
-                          </div>
-                        </td>
-                        <td>
-                          <p class="fs-3 fw-normal mb-0">Mita</p>
-                        </td>
-                        <td>
-                          <p class="fs-3 fw-normal mb-0">
-                            Langganan Paket
-                          </p>
-                        </td>
-                        <td>
-                          <span
-                            class="badge bg-light-danger rounded-pill text-danger px-3 py-2 fs-3"
-                            >Belum Bayar</span
-                          >
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
+                    <button type="submit" name="simpan" class="btn btn-primary">Tambah</button>
+                  </form>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div class="py-6 px-6 text-center">
-          <p class="mb-0 fs-4">Design and Developed by <a href="https://adminmart.com/" target="_blank" class="pe-1 text-primary text-decoration-underline">AdminMart.com</a></p>
         </div>
       </div>
     </div>
